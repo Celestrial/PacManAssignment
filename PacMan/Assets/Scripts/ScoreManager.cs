01de56a8834f5266be2b9238855585baef035aab
celestrial <sean.celestrial@gmail.com>
2015-04-04 03:10:33 -0400

    Basically finished, probably should add lives. Scoring works, ghost kill works !!!NEED TO ADD END GAME STATE!!!

---
+++ PacMan/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Comp476A3
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        Text text;
+        public static int pacmanScore = 0;
+        public static int pacwomanScore = 0;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            text = GetComponent<Text>();
+            if (transform.name == "PacManScore")
+                text.text = ""+Game.pacManScore;
+            if (transform.name == "PacWomanScore")
+                text.text = ""+Game.pacWomanScore;
+	    }
+    }
+}
